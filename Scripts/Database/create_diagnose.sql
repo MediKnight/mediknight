7ce6c3f4e08265a53c79dbbc84245e68631784aa
jb <>
2003-06-16 12:33:15 +0000

    Sync.

--- Scripts/Database/create_diagnose.sql
+++
@@ -1,6 +0,0 @@
-create table tagesdiagnose (
-	id         int(11) default 0 auto_increment primary key,
-	patient_id int(11) default 0,
-	datum      date default '0000-00-00',
-	text       longtext default null
-);
--- Scripts/Database/create_verordnung.sql
+++
@@ -1,6 +0,0 @@
-create table verordnung (
-	id          int(11) default 1 auto_increment primary key,
-	diagnose_id int(11) default 0,
-	datum       date default '0000-00-00',
-	object      longtext default null
-);
